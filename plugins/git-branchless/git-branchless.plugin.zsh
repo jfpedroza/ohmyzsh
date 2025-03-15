967a01359f7372b4a9c4d1c9fce8eb77b4475b39
Jhon Pedroza <jhon@pedroza.me>
2025-03-15 03:26:33 -0500

    feat(git-branchless): add `git-branchless` plugin

---
+++ plugins/git-branchless/git-branchless.plugin.zsh
@@ -0,0 +1,10 @@
+# Aliases
+alias gsl='git smartlog'
+alias gsls="git smartlog 'stack()'"
+alias gmv='git move'
+alias gmvm='git move -d $(git_main_branch)'
+alias gmvmb='git move -d $(git_main_branch) -b'
+alias grtk='git restack'
+alias gnt='git next'
+alias gamd='git amend'
+alias gsmt='git submit'
